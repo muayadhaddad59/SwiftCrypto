b0762d5786a77b6339f4f7e73d4233afe9069303
muayadhaddad59 <muayadhaddad@yahoo.com>
2024-04-23 20:30:40 -0400

    added Launch Screen to the project

---
+++ SwiftfulCrypto/Core/Launch/Views/LaunchView.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchView.swift
+//  SwiftfulCrypto
+//
+//  Created by Muayad El-Haddad on 2024-04-23.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LaunchView()
+}
